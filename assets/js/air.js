6da6ba06d1d468623319ae481bd10bea1af5d9be
c-tatsuhiko <t-chiba@nixe-inc.com>
2017-11-17 19:32:18 +0900

    大幅に変更

--- assets/js/air.js
+++ assets/js/air.js
@@ -2,9 +2,24 @@ $(function () {
     $("#header").load("/header/header.html");
 })
 
+//詳細オプションクリック時
 var viewModel = function () {
+    var flg = "default";
     this.myHandler = function () {
-        alert("test");
+        $('#accordion').slideToggle("fast");
+
+        //jQueryで実装
+        if (flg == "default") {
+            $('#DetailOption').text("－");
+            flg = "change";
+        } else {
+            $('#DetailOption').text("＋");
+            flg = "default";
+
+        }
+
+        //knockoutで実装
+
     }
 };
-ko.applyBindings(viewModel);
+ko.applyBindings(new viewModel);
